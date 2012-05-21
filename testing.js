c5b4c8ee1ec1fb449502c9fb0fe1b40ba096dc6b
Plato <therealplato@gmail.com>
2012-05-21 14:25:00 -0400

    added nano2.js with a more complicated newid()

--- testing.js
+++ testing.js
@@ -1,17 +1,38 @@
-var nano=require('./nano.js')('http://localhost:5984');
-var mycouchdb = nano.use('foo_db');
-
-mycouchdb.newid(function(err, id_string){
-  if(!err){console.log('mycouchdb.newid: '+id_string)}
-  else {console.log('error: \n'+err)};
-});
-nano.newid(function(err, id_string){
-  if(!err){console.log('nano.newid: '+id_string)}
-  else {console.log('error: \n'+err)};
+var nano=require('./nano2.js')('http://localhost:5984');
+var mycouchdb = null;
+nano.db.create('foodb', function(err, body) {
+  if(!err) {console.log('Created db /foodb')};
+  mycouchdb = nano.use('foodb');
+  runTests();
 });
 
+runTests = function() {
+  console.log('Trying nano.relax(\'_uuids\'):');
+  nano.relax('_uuids', function(err, body) {
+    if(!err){console.log('  nano.relax(\'_uuids\'):\n    '
+       +JSON.stringify(body.uuids));}
+    else {console.log('  nano.relax(\'_uuids\') error: \n'+err)};
+  });
+
+  console.log('Trying mycouchdb.newid(fn):');
+  mycouchdb.newid(function(err, id_string){
+    if(!err){console.log('  mycouchdb.newid():\n    '
+      +JSON.stringify(id_string))}
+    else {console.log('  mycouchdb.newid() error: \n    '+err)};
+  });
+
+  console.log('Trying nano.newid(2,fn):')
+  nano.newid(2, function(err, id_strings_arr){
+    if(!err){console.log('  nano.newid(2,fn):\n    '
+      +JSON.stringify(id_strings_arr))}
+    else {console.log('  nano.newid(2,fn) error: \n    '+err)};
+  });
 
 
-
-
-
+  console.log('Trying mycouchdb.newid(1,2,1,fn):')
+  mycouchdb.newid(1,2,1,function(err, id_strings_array){
+    if(!err){console.log('  mycouchdb.newid(1,2,1,fn):\n    '
+      +JSON.stringify(id_strings_array))}
+    else {console.log('  mycouchdb.newid(1,2,1,fn) error:\n    '+err)};
+  });
+};
